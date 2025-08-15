aef0a1f6918ce1c98eed2a5bd3999840df108fb8
Yongfu Lin <alfielin0709@gmail.com>
2025-08-15 22:25:32 +1200

    Styling the User Interface

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,15 +1,24 @@
 import { Box, Container, CssBaseline } from "@mui/material";
 import Navbar from "./Navbar";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import HomePage from "../../features/home/HomePage";
 
 function App() {
+  const location = useLocation();
+
   return (
     <Box sx={{ bgcolor: "#eeeeee", minHeight: "100vh" }}>
       <CssBaseline />
-      <Navbar />
-      <Container maxWidth="xl" sx={{ mt: 3 }}>
-        <Outlet />
-      </Container>
+      {location.pathname === "/" ? (
+        <HomePage />
+      ) : (
+        <>
+          <Navbar />
+          <Container maxWidth="xl" sx={{ mt: 3 }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
     </Box>
   );
 }
